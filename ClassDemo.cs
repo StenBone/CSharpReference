feb69786837e2de516a90b10ecfe23beb75f70f4
Ben Stone <ben.stone@canal-ins.com>
2021-05-18 11:46:40 -0400

    Started ClassDemo

---
+++ ClassDemo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpReference {
+    /// <summary>
+    /// Class is a Type with Methods, Properties and Fields
+    /// </summary>
+    class ClassDemo {
+    }
+}
