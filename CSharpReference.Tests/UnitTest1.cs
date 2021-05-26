fb4cc4e6883b3cd20c6322091c5699925f5874a2
Ben Stone <ben.stone@canal-ins.com>
2021-05-26 16:39:07 -0400

    Added reference to CSharpReference from tests project. Added the 3 a's of testing.

--- CSharpReference.Tests/UnitTest1.cs
+++ CSharpReference.Tests/UnitTest1.cs
@@ -5,7 +5,9 @@ namespace CSharpReference.Tests {
     public class UnitTest1 {
         [Fact]
         public void Test1() {
-
+            //arrange
+            //act
+            //assert
         }
     }
 }
